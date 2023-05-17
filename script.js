433205b2fa1609139ab5516b020b5a2e6a49e891
Hendrypoul <106142503+Hendrypoul@users.noreply.github.com>
2023-05-17 23:32:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ class Rectangle {
 		this._width = width;
 		this._height = height;
 	}
-	get Width() {
+	get width() {
 		return this._width;
 	}
-	get Height() {
+	get height() {
 		return this._height;
 	}
 	getArea() {
